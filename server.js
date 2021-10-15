7c86ac2f2c30f085a27f9d634462ff37f3c0e18f
Ayer <ayer989@gmail.com>
2021-10-16 00:49:45 +0400

    s

--- server.js
+++ server.js
@@ -1,52 +1,35 @@
-var app = require("express")();
-var mysql = require("mysql");
-var http = require('http').Server(app);
-var io = require("socket.io")(http);
-
-/* Creating POOL MySQL connection.*/
-
-var pool = mysql.createConnection({
-host     : '127.0.0.1',
-user     : 'root',
-password : '',
-database : 'eduportal'
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+var players = {};
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
-
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  // create a new player and add it to our players object
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+  };
+  // send the players object to the new player
+  socket.emit('currentPlayers', players);
+  // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+  // when a player disconnects, remove them from our players object
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+    // remove this player from our players object
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit('disconnect', socket.id);
+  });
 });
-
-
-io.on('connection', function(socket) {
-    console.log("A user is connected");
-        get_cs_name(function(res,cs_name) {
-            if (res) {
-                io.emit('get_cs_name', cs_name);
-            } else {
-                io.emit('error');
-            }
-        });
-});
-
-var get_cs_name = function(callback) {
-    pool.getConnection(function(err, connection) {
-        if (err) {
-            connection.release();
-            callback(false);
-            return;
-        }
-        connection.query("SELECT cs_name FROM course_master where cs_id = 4", function(err, rows) {
-            connection.release();
-            if (!err) {
-                callback(true,rows[0].cs_name);
-            }
-        });
-        connection.on('error', function(err) {
-            callback(false,null);
-        });
-    });
-}
-
-http.listen(3000, function() {
-    console.log("Listening on 3000");
+server.listen(8081, function () {
+  console.log(`Listening on ${server.address().port}`);
 });
